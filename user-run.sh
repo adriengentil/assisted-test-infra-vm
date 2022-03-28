d8e5774cfb833790405d8e4a4dcfa63fe1de73ef
Adrien Gentil <agentil@redhat.com>
2022-03-28 18:11:35 +0200

    more env

--- user-run.sh
+++ user-run.sh
@@ -3,9 +3,14 @@
 set -xeuo pipefail
 
 export REPO_DIR="/home/assisted"
+mkdir -p "${REPO_DIR}/minikube_home"
+export MINIKUBE_HOME="${REPO_DIR}/minikube_home"
+
 export TEST_FUNC=test_install
 export TEST_SUITE=full
 export TEST_TEARDOWN=false
+export CHECK_CLUSTER_VERSION=True
+export INSTALLER_KUBECONFIG="${REPO_DIR}/build/kubeconfig"
 
 PULL_SECRET="$(cat /tmp/pull-secret.json)"
 export PULL_SECRET
@@ -15,7 +20,7 @@ export PATH="${PATH}:/usr/local/bin" # required to access skipper
 cd "${REPO_DIR}"
 make create_full_environment run test_parallel
 
-export KUBECONFIG=/home/assisted/build/kubeconfig
-export KUBECONFIG=$(find ${KUBECONFIG} -type f)
+KUBECONFIG=$(find ${INSTALLER_KUBECONFIG} -type f)
+export KUBECONFIG
 export TEST_FUNC=test_late_binding_kube_api_sno
 make deploy_assisted_operator test_kube_api_parallel
